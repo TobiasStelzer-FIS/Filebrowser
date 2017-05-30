5368b9dc5c794485124d8fced577a570276790a5
Tobias Stelzer <t.stelzer@fis-gmbh.de>
2017-05-30 15:47:01 +0200

    Unit Tests

--- WebContent/filebrowser/model/formatter.js
+++ WebContent/filebrowser/model/formatter.js
@@ -15,19 +15,21 @@ sap.ui.define([
 			},
 			
 			formatFilesize: function (iSize) {
-				if (!iSize)
+				if (iSize === undefined)
 					return "";
 					
 				var MB = 1000000;
 				var KB = 1000;
 				if (iSize >= 1 * MB) {
-					// Size is bigger or equal 10 MB
+					// Size is bigger or equal 1 MB
 					return (iSize / MB).toFixed(1) + " MB";
 				} else if (iSize >= 0.1 * KB) {
-					// Size is bigger or equal 0.1 KB but lesser than 10 MB
+					// Size is bigger or equal 0.1 KB but lesser than 1 MB
 					return (iSize / KB).toFixed(1) + " KB";
-				} else {
+				} else if (iSize > 0) {
 					return "<0.1 KB";
+				} else {
+					return "0 B";
 				}
 			}
 		};
